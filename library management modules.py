edc36ab59c9cc8df5cd6e18ddce3d202f5e01a4f
titusthuo <121286467+titusthuo@users.noreply.github.com>
2024-03-18 23:02:40 +0300

    Update library management modules.py

--- library management modules.py
+++ library management modules.py
@@ -4,7 +4,7 @@ import mysql.connector
 mydb = mysql.connector.connect(
     host="localhost",
     user="root",
-    password="107220Mwangi@",
+    password="your_password",
     database="library_management_system"
 )
 
